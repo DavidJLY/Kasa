6e2e82b9e2ca719c6ea117ef9ee61ef0de6f5af0
DavidJLY <dj06700@gmail.com>
2024-08-08 12:47:07 +0200

    maj css

--- src/Components/ErrorBoundary/ErrorBoundary.jsx
+++ src/Components/ErrorBoundary/ErrorBoundary.jsx
@@ -8,18 +8,15 @@ class ErrorBoundary extends Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Mettre à jour l'état pour indiquer qu'une erreur s'est produite
     return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
-    // Vous pouvez aussi enregistrer l'erreur dans un service de reporting
     console.log(error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      // Rediriger vers la page d'erreur
       return <Navigate to="/errorpage" />;
     }
 
